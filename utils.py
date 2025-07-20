b4098e61e18c528dfa4ca9867b86bb6db2220034
adil24689 <mrjohn7162@gmail.com>
2025-07-21 02:21:57 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,9 @@
-# utils.py
-
 from config import ADMIN_IDS
-from db import get_user
+import db
 
 def is_admin(user_id):
-    return user_id in ADMIN_IDS
+    return int(user_id) in ADMIN_IDS
 
-def user_balance(telegram_id):
-    user = get_user(telegram_id)
-    if user:
-        return user[3]  # balance column
-    return 0
+def user_balance(user_id):
+    user = db.get_user(user_id)
+    return user[3] if user else 0
