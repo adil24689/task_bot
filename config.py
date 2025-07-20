0d61abe01b620e757a658b8aac60b0e1001c4a00
adil24689 <mrjohn7162@gmail.com>
2025-07-21 02:21:28 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [8121844150]
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+PAYMENT_NUMBERS = {
+    "bkash": os.getenv("BKASH_NUMBER", "017XXXXXXXX"),
+    "nagad": os.getenv("NAGAD_NUMBER", "018XXXXXXXX"),
+}
+DB_NAME = "data.sqlite3"
